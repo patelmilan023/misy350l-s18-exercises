229b0589c30e4d1353b6fe79b542b54499e530db
patelmilan023 <patelmilan023@gmail.com>
2018-04-05 10:44:23 -0400

    js exercise

---
+++ 4-5-18/my-code.js
@@ -0,0 +1,15 @@
+blist =  ["Make money", "get good grades","visit family in india"];
+console.log(blist.length);
+blist.push("get a beer");
+console.log(blist.length);
+
+console.log(blist.length);
+console.log(blist);
+
+/* for loop
+multiple line comment
+can make blist.length = 4 because thats what it is.
+*/
+for (let i=0; i<blist.length; i++) {
+  console.log(blist[i]);
+}
